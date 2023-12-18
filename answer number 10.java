e94c51f1f8617a67e39da01acc6870b8c31bd750
Or Agasi <67040075+Or-Ag@users.noreply.github.com>
2023-12-18 17:45:42 +0200

    Update answer number 10.java

--- answer number 10.java
+++ answer number 10.java
@@ -16,3 +16,23 @@ public class Main {
         System.out.println("the total of the number is" + " "+ sum);
     }
 }
+
+//question B
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        System.out.println("This is a program that prints the following series and the sum of the series: 1/1 , 1/2 , 1/3 , 1/4 , .. , 1/N");
+        for(int i=99; i>=1; i--)
+        {
+            double f = 1.0 / (3 * i);
+            System.out.println(f + " ");
+            sum+=f;
+
+        }
+        System.out.println("the total of the number is" + " "+ sum);
+    }
+}
